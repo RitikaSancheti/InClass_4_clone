b4008024eaa2fe6d396f7e0a3720b0710c3274b5
Ritika Sancheti <sanchetr@sheridancollege.ca>
2026-02-05 18:58:28 -0500

    Division method added

---
+++ InClass_4_clone/src/main/java/ice_2/inclass_4_clone/InClass_4_clone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ice_2.inclass_4_clone;
+
+/**
+ *
+ * @author ritikasancheti
+ */
+public class InClass_4_clone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
